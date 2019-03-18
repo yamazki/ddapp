d0e8178385fb6910ddc1dd5620de70ce910e72b4
Keisuke Yamazaki <ma17123@shibaura-it.ac.jp>
2019-03-18 23:10:20 +0900

    ドラッグアンドドロップで矢印を追加させることができたが,なんか違うので他のライブラリを探す

--- public/js/canvas/canvas-object/canvas-object-event.js
+++ public/js/canvas/canvas-object/canvas-object-event.js
@@ -1,5 +1,5 @@
 import {CanvasObjectView} from './canvas-object-view.js'
-import {LeaderLine} from '../../lib/leader-line.min.js'
+import {LeaderLine} from '../../lib/leader-line/leader-line.min.js'
 
 export class CanvasObjectEvent {
   
@@ -15,7 +15,10 @@ export class CanvasObjectEvent {
   static drop(e) {
     console.log(window.canvas.draggedElement);
     // 始点をdraggedelementの中心,終点をthisとした線を引く
-    
+    console.log(LeaderLine);
+    console.log(window.canvas.draggedElement);
+    console.log(this);
+    new LeaderLine(window.canvas.draggedElement, this);
     
     e.stopPropagation();
   }
